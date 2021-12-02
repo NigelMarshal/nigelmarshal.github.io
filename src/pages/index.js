6ae875a3ff40483c3a7499578adef863bc9eb0e7
NigelMarshal <nigelmarshal@gmail.com>
2021-12-03 01:53:41 +0400

    feat(App): Starter files set up

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  AboutSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Nigel Marshal - Portfolio" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
+        <InterestsSection sectionId="details" heading="Details" />
+        <ProjectsSection sectionId="features" heading="Built-in Features" />
+        <ContactSection sectionId="github" heading="Issues?" />
+      </Page>
+    </>
+  );
+}
