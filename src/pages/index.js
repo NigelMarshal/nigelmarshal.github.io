8e836f62db9af895527b28d11d4ba3563d167e8d
NigelMarshal <nigelmarshal@gmail.com>
2021-12-06 01:30:04 +0400

    chore(App): Title change

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Nigel Marshal - Portfolio" />
+      <Seo title="Nigel Marshal - Frontend Engineer" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About Me" />
