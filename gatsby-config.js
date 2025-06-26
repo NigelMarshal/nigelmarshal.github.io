578e67f65ab4df348a5a6609ce0e969bb5995512
Nigel Marshal <nigelmarshal@gmail.com>
2025-06-26 17:45:28 +0100

    Comment out blogSettings in gatsby-config.js
    The blogSettings configuration has been commented out in the gatsby-config.js file, possibly to disable custom blog path and article prefixing without deleting the configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,10 +32,10 @@ module.exports = {
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
-        blogSettings: {
-          path: "/blog", // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-        },
+        // blogSettings: {
+        //   path: "/blog", // Defines the slug for the blog listing page
+        //   usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        // },
       },
     },
     {
